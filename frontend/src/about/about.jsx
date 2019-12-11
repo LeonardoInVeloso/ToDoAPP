750f7bc1d26e1f25e1c95f9f46141084cedeee01
Leonardo Inocencio Veloso <318005@ptin.corppt.com>
2019-12-11 19:11:43 -0300

    Running App

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+ <div>
+     <PageHeader name='Sobre' small='Nós'></PageHeader>
+     <h2>Nossa Historia</h2>
+     <p>teste</p>
+     <h2>Missão e Visão</h2>
+     <p>Teste</p>
+     <h2>Imprensa</h2>
+ </div>
+)
